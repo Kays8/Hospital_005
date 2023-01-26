d59bfc7eb0bb16d50a5cee688402653765b16ff0
Kays8 <thirayut1379@gmail.com>
2023-01-26 10:00:59 +0700

    StartClass

--- service/connect.php
+++ service/connect.php
@@ -9,7 +9,6 @@
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Success! ! !" ;
 
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
